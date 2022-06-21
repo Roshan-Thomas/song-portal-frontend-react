301b04f82d807f27f70aaed07ee65d1416d93942
Roshan-Thomas <roshan.thomas@qatar.tamu.edu>
2022-06-21 20:24:55 +0300

    Connect wallet to web app

--- src/App.js
+++ src/App.js
@@ -1,18 +1,63 @@
-import * as React from "react";
-import { ethers } from "ethers";
+import React, { useEffect, useState } from "react";
 import './App.css';
 
 export default function App() {
+  const [currentAccount, setCurrentAccount] = useState("");
 
-  const wave = () => {
+  const checkIfWalledIsConnected = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        console.log("Make sure you have metamask!");
+      } else {
+        console.log("We have the ethereum object", ethereum);
+      }
+
+      // check if we're authorized to acces the user's wallet
+      const accounts = await ethereum.request({ method: "eth_accounds" });
+
+      if (accounts.length !== 0) {
+        const account = accounts[0];
+        console.log("Found an authorized account:", account);
+        setCurrentAccount(account)
+      } else {
+        console.log("No authorized account found")
+      }
+    } catch (error) {
+      console.log(error);
+    }
     
   }
   
+  const connectWallet = async () => {
+    try {
+      const {ethereum} = window;
+
+      if (!ethereum) {
+        alert("Get MetaMask!");
+        return;
+      }
+
+      const accounts = await ethereum.request({ method: "eth_requestAccounts" });
+
+      console.log("Connected", accounts[0]);
+      setCurrentAccount(accounts[0]);
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    checkIfWalledIsConnected();
+  }, [])
+
   return (
     <div className="mainContainer">
 
       <div className="dataContainer">
         <div className="header">
+        {/* eslint-disable-next-line*/}
         👋 Hey there!
         </div>
 
@@ -20,9 +65,18 @@ export default function App() {
         I am farza and I worked on self-driving cars so that's pretty cool right? Connect your Ethereum wallet and wave at me!
         </div>
 
-        <button className="waveButton" onClick={wave}>
+        <button className="waveButton" onClick={null}>
           Wave at Me
         </button>
+
+        {/*
+        * If ther is no curretAccount render this button
+    */}
+        {!currentAccount && (
+          <button className="waveButton" onClick={connectWallet}>
+            Connect Wallet
+          </button>
+        )}
       </div>
     </div>
   );
